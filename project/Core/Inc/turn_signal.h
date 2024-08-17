bcd78ae1b922827c7973f3d014ca85749a5987c7
Sergio Abraham Botina Josa <sbotinaj@unal.edu.co>
2024-08-16 19:22:13 -0500

    [Core: turn signal lib create]

---
+++ project/Core/Inc/turn_signal.h
@@ -0,0 +1,18 @@
+/*
+ * led_lib.h
+ *
+ *  Created on: Aug 16, 2024
+ *      Author: 57316
+ */
+
+#ifndef INC_TURN_SIGNAL_H_
+#define INC_TURN_SIGNAL_H_
+
+#include <stdint.h>
+
+void turn_signal_left(void);
+void turn_signal_right(void);
+void turn_signal_hazard(void);
+
+
+#endif /* INC_TURN_SIGNAL_H_ */
